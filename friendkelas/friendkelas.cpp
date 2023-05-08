c83abeedf1b72c2f086f8d0cc198163da8f007c6
rezaikram <muhammadikramriza@gmail.com>
2023-05-08 13:52:14 +0700

    membuat implementasi metod diluar kelas

--- friendkelas/friendkelas.cpp
+++ friendkelas/friendkelas.cpp
@@ -20,5 +20,22 @@ public:
 	void displayAll(orang& a);
 };
 
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+void siswa::setId(int pId) {
+	id = pId;
+}
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayAll(joko);
+	return 0;
+}
 
 
