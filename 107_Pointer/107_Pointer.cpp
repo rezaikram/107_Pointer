ad797ffadd62f2ce2e4e72e1c35d642ef4f5b36a
rezaikram <muhammadikramriza@gmail.com>
2023-05-08 13:38:24 +0700

    membuat kelas mahasiswa

--- 107_Pointer/107_Pointer.cpp
+++ 107_Pointer/107_Pointer.cpp
@@ -8,17 +8,3 @@ class mahasiswa {
 			cout << "No Induk =" << nim << endl;
 			}
 };
-
-int main() {
-	mahasiswa mhs{1};		// Object mhs
-	mhs.showNim();			// Member access Operator
-
-	mahasiswa& refMhs = mhs;	// Pointer referece
-	refMhs.nim = 2;			// Member Accsess Operator
-	mhs.showNim();
-
-	mahasiswa* pMhs = &mhs;		// pointer deference pMhs
-	pMhs->nim = 3;		// Arrow Operator
-	mhs.showNim();
-	return 0;
-}
