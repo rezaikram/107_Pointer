914c95b10ece14623e3a960f9809f5b82bf02c54
rezaikram <muhammadikramriza@gmail.com>
2023-05-08 13:36:03 +0700

    Add project files.

---
+++ 107_Pointer/107_Pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+	public:
+		int nim;
+		void showNim() {
+			cout << "No Induk =" << nim << endl;
+			}
+};
+
+int main() {
+	mahasiswa mhs{ 1 };		// Object mhs
+	mhs.showNim();			// Member access Operator
+
+	mahasiswa& refMhs = mhs;	// Pointer referece
+	refMhs.nim = 2;			// Member Accsess Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		// pointer deference pMhs
+	pMhs->nim = 3;		// Arrow Operator
+	mhs.showNim();
+	return 0;
+}
